7c776514fd881566b7aad879d9cc8b92cc1d62f3
mitya <DmitryEjov@gmail.com>
2019-02-04 23:38:15 +0300

    First view added

--- news/views.py
+++ news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from news.models import Post
 
 # Create your views here.
 
 def news_list(request):
-    return render(request, 'news/news_list.html', {})
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'news/news_list.html', {'posts': posts})
